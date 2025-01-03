418b6131c645e456c237b3418fc5ce577b47c678
Harry-Daniel <hayiborkomla@gmail.com>
2025-01-03 19:08:36 +0000

    returned lowercase

--- Day3/project.py
+++ Day3/project.py
@@ -1,18 +1,18 @@
 print("Welcome to Treasure Island.\n Your mission is to find the treasure.")
 
-direction = input("You're at a crossroads. Do you go left or right?")
+direction = input("You're at a crossroads. Do you go left or right?").lower()
 
 if direction=='left':
-    direction= input("You're at river.\n Do you swim or wait?")
+    direction= input("You're at river.\n Do you swim or wait?").lower()
 
     if direction== 'wait':
-        direction=input("You reach an Island with 3 different colored doors.Which door do you take? Yellow, Red or Blue?")
+        direction=input("You reach an Island with 3 different colored doors.Which door do you take? Yellow, Red or Blue?").lower()
 
-        if direction=='Yellow':
+        if direction=='yellow':
             print("You Win!!")
-        elif direction=='Red':
+        elif direction=='red':
             print("You were burned by fire.\nGame Over.")
-        elif direction=='Blue':
+        elif direction=='blue':
             print("You were eaten by beasts.\nGame Over.")
             
         else:
