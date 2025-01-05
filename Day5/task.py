3eed344bf60a9a429cfe3f6485fce5c8bf7f4fef
Harry-Daniel <hayiborkomla@gmail.com>
2025-01-05 21:04:05 +0000

    Day 5, I got beaten for the first time. Nice one

---
+++ Day5/task.py
@@ -0,0 +1,33 @@
+# fruits=["Apple","Peaches","Pear"] 
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit+ " pie")
+# print(fruits) 
+
+# student_score=[150,142,185,120,171,184,149,24,59,68,199,78,65,89,86,55,91,64,89]
+
+# sum
+# total_exam_score=sum(student_score)
+# print(total_exam_score)
+
+# sum=0
+# for score in student_score:
+#     sum+=score
+
+# print(sum)
+
+
+# print(max(student_score))
+
+# maximum=0
+# for score in student_score:
+#     if score >maximum:
+#         maximum=score
+
+# print(maximum)
+
+    # For with Range function0
+sum=0
+for number in range(1,101):
+    sum+=number
+print(sum)
