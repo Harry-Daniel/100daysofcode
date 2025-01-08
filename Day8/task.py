453e359842789daf0ff5a2abf15db27e7010aa0d
Harry-Daniel <hayiborkomla@gmail.com>
2025-01-08 18:19:53 +0000

    Day 8 loading

---
+++ Day8/task.py
@@ -0,0 +1,22 @@
+# def greet():
+#     print("Hello")
+#     print("Hi")
+#     print("How are ya?")
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"Hi {name}")
+#     print(f"How are ya {name}?")
+
+# greet_with_name("Bianca")
+
+
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"How is it like in {location}")
+
+greet_with(location="Accra",name="Mawuko")
+
+print("Mawuko"+"Mmabilla")
