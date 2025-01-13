46cb64d24084ffcc73e9fc5a559da834bf6a62c4
Harry-Daniel <hayiborkomla@gmail.com>
2025-01-13 22:59:02 +0000

    Using a Debugger

--- Day13/task.py
+++ Day13/task.py
@@ -24,9 +24,24 @@
 # if age>18:
 #     print(f"You can drive at age {age}.")
 
-words_per_page = 0
+# words_per_page = 0
 
-pages=int(input("Number of words per page: "))
-words_per_page = int(input("Number of words per page: "))
-total_words= pages * words_per_page
-print(total_words)
+# pages=int(input("Number of words per page: "))
+# words_per_page = int(input("Number of words per page: "))
+# total_words= pages * words_per_page
+# print(total_words)
+import random
+import maths
+
+def mutate(a_list):
+    b_list=[]
+    new_item=0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1,3)
+        new_item = maths.add(new_item, item)
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate([1,2,3,5,8,13])
