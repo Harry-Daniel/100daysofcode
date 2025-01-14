9f8de37a6e45f39838af132d9a95e1f48765947e
Harry-Daniel <hayiborkomla@gmail.com>
2025-01-14 18:54:12 +0000

    Day14 underway

---
+++ Day14/data.py
@@ -0,0 +1,44 @@
+data=[
+    {
+        'name': 'Instagram',
+        'follwer_count': 346,
+        'description':'Social media platform',
+        'country': 'United States',
+     },
+    {
+        'name': 'Christiano Ronaldo',
+        'follwer_count': 215,
+        'description':'Footballer',
+        'country': 'Portugal',
+     },
+    {
+        'name': 'Ariana Grande',
+        'follwer_count': 183,
+        'description':'Musician and actress',
+        'country': 'United States',
+     },
+    {
+        'name': 'Dwayne Johnson',
+        'follwer_count': 181,
+        'description':'Actor and professional wrestler',
+        'country': 'United States',
+     },
+    {
+        'name': 'Selina Gomez',
+        'follwer_count': 174,
+        'description':'Musician and actress',
+        'country': 'United States',
+     },
+    {
+        'name': 'Kylie Jenner',
+        'follwer_count': 172,
+        'description':'Reality TV personality and buisnesswoman and Self-Made Billionaire',
+        'country': 'United States',
+     },
+    {
+        'name': 'Kim Khardashian',
+        'follwer_count': 167,
+        'description':'Reality TV personality and buisnesswoman',
+        'country': 'United States',
+     },
+]
