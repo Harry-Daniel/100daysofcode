229bee93609ba1ea65451f56dc84a2bfb0aeaebb
Harry-Daniel <hayiborkomla@gmail.com>
2025-02-02 22:03:03 +0000

    Creat snake class, move to oop

--- Day20/project.py
+++ Day20/project.py
@@ -1,19 +1,24 @@
-from turtle import Screen,Turtle
+from turtle import Screen
+from snake import Snake
+import time
 
 screen= Screen()
 
 screen.setup(width=600,height=600)
 screen.bgcolor("black")
 screen.title("My snake game")
+screen.tracer(0)
 
-position=0
-for tim in range (3):
-    tim=Turtle('square')
-    tim.color("white")
-    tim.backward(position)
-    position+=20
+snake=Snake()
 
 
+game_is_on=True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+    
 
 
 screen.exitonclick()
