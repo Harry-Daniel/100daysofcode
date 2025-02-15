cae24fbb7ff241314665f2b9d90b834b54a7cec8
Harry-Daniel <hayiborkomla@gmail.com>
2025-02-15 15:36:23 +0000

    getting started on project

--- Day20_21/scoreboard.py
+++ Day20_21/scoreboard.py
@@ -7,7 +7,9 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score=0
-        self.high_score=0
+        with open("Day20_21/data.txt") as file:
+            self.high_score=int(file.read())
+        
         self.goto(0,250)
         self.color("white")
         self.hideturtle()
@@ -21,6 +23,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score>self.high_score:
             self.high_score = self.score
+        with open("Day20_21/data.txt",mode='w') as file:
+            file.write(str(self.high_score))
+        
         self.score=0
         self.refresh()
  
