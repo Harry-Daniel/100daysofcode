d8a137e3dcbbe62a6d910e78cee0810e0f512302
Harry-Daniel <hayiborkomla@gmail.com>
2025-02-17 17:02:48 +0000

    closing project starting

--- Day25/main.py
+++ Day25/main.py
@@ -5,7 +5,7 @@
 #     print(data)
 
 
-# with open ("Day25/weather_data.csv") as file:
+# with open ("Day25/weather_data.zcsv") as file:
 #     data= csv.reader(file)
 #     data_list=[]
 #     temperatures=[]
@@ -17,5 +17,35 @@
 
 import pandas
 
-data=pandas.read_csv("Day25/weather_data.csv")
-print(data["temp"][0])
+data=pandas.read_csv("Day25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20250217.csv")
+# print(type(data["temp"][0]))
+
+# data_dict=data.to_dict()
+
+# print(data_dict)
+
+# temp_list = data["temp"].to_list()
+
+# print(data[data.temp==data.temp.max()])
+
+grey= data[data['Primary Fur Color']== "Gray"]
+no_grey=len(grey)
+
+red= data[data['Primary Fur Color']== "Cinnamon"]
+no_red=len(red)
+
+black= data[data['Primary Fur Color']== "Black"]
+no_black=len(black)
+print(no_black)
+
+
+
+# Create a dataframe from scratch
+
+data_dict={
+    "Fur Color":["grey","red","black"],
+    "Count":[no_grey,no_red, no_black]
+}
+
+data= pandas.DataFrame(data_dict)
+data.to_csv("Day25/squirrel_count.csv")
