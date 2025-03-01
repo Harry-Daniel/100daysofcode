dd6e99d587fcb763b02f1b0fa12bef5b82140154
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-01 16:03:06 +0000

    Completing application user interface

--- Day28/main.py
+++ Day28/main.py
@@ -21,11 +21,24 @@ window=Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg=YELLOW)
 
+title_label=Label(text="Timer",font=(FONT_NAME,50),bg=YELLOW,fg=GREEN)
+title_label.grid(column=1,row=0)
+
 canvas= Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
 tomato_img=PhotoImage(file="Day28/tomato.png")
 canvas.create_image(100,112,image=tomato_img)
 canvas.create_text(100,130,text='00:00', fill="white", font=(FONT_NAME,35,"bold"))
-canvas.pack()
+canvas.grid(column=1,row=1)
+
+start=Button(text='Start')
+start.grid(column=0,row=2)
+
+reset=Button(text='Reset')
+reset.grid(column=3,row=2)
+
+
+checksigns=Label(text="✔️",bg=YELLOW,fg=GREEN)
+checksigns.grid(column=1,row=3)
 
 
 
