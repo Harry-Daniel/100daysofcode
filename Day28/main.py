f6da6af1c80249709747d37214a4bd26b79608d3
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-01 17:16:39 +0000

    adding count down mechanism

--- Day28/main.py
+++ Day28/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -12,8 +13,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5*60)
+
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    count_min= math.floor(count/60)
+    count_sec=count%60
+
+
+
+
+    canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
+    if count>0:
+        window.after(1000,count_down,count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -21,16 +36,18 @@ window=Tk()
 window.title("Pomodoro")
 window.config(padx=100,pady=50,bg=YELLOW)
 
+
 title_label=Label(text="Timer",font=(FONT_NAME,50),bg=YELLOW,fg=GREEN)
 title_label.grid(column=1,row=0)
 
 canvas= Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
+
 tomato_img=PhotoImage(file="Day28/tomato.png")
 canvas.create_image(100,112,image=tomato_img)
-canvas.create_text(100,130,text='00:00', fill="white", font=(FONT_NAME,35,"bold"))
+timer_text=canvas.create_text(100,130,text='00:00', fill="white", font=(FONT_NAME,35,"bold"))
 canvas.grid(column=1,row=1)
 
-start=Button(text='Start')
+start=Button(text='Start',command=start_timer)
 start.grid(column=0,row=2)
 
 reset=Button(text='Reset')
