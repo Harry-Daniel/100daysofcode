853bf554fc6046b36fd52587ce25f35b685281a5
Harry-Daniel <hayiborkomla@gmail.com>
2025-03-04 21:30:18 +0000

    Day 29 working with images and settingup the canvas

--- Day29/main.py
+++ Day29/main.py
@@ -1,5 +1,24 @@
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+
+window=Tk()
+window.title("Password Manager")
+window.config(padx=20,pady=20)
+
+canvas= Canvas(width=200, height=200)
+
+lockImage=PhotoImage(file="Day29/logo.png")
+canvas.create_image(100,100,image=lockImage)
+canvas.pack()
+
+
+
+
+window.mainloop()
