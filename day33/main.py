4ee7947bad0ce8471ed99df344b9bf339ef27e3b
Harry-Daniel <hayiborkomla@gmail.com>
2026-02-01 22:45:17 +0000

    Working with responses http codes, Exceptions & Json data

--- day33/main.py
+++ day33/main.py
@@ -1,5 +1,13 @@
 import requests
 
 response= requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
 
-print(response)
+data = response.json()["iss_position"]["longitude"]
+
+longitude=response.json()["iss_position"]["longitude"]
+latitude=response.json()["iss_position"]["latitude"]
+
+iss_position=(longitude,latitude)
+
+print(iss_position)
