80d298dd0aede609ec52e0f99631e9db2fe5e712
Harry-Daniel <hayiborkomla@gmail.com>
2026-06-10 11:55:56 +0000

    Understanding API Parameters: Match Sunset Times with the Current Time

--- day33/main.py
+++ day33/main.py
@@ -1,13 +1,38 @@
+# import requests
+
+MY_LAT= 5.603717
+MY_LNG= -0.186964
+
+# response= requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()["iss_position"]["longitude"]
+
+# longitude=response.json()["iss_position"]["longitude"]
+# latitude=response.json()["iss_position"]["latitude"]
+
+# iss_position=(longitude,latitude)
+
+# print(iss_position)
+
 import requests
+from datetime import datetime
 
-response= requests.get(url="http://api.open-notify.org/iss-now.json")
+parameters={
+    "lat":MY_LAT,
+    "lng":MY_LNG,
+    "formatted":0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
 response.raise_for_status()
+data= response.json()
+sunrise=data['results']['sunrise'].split("T")[1].split(":")[0]
+sunset=data['results']['sunset'].split("T")[1].split(":")[0]
 
-data = response.json()["iss_position"]["longitude"]
 
-longitude=response.json()["iss_position"]["longitude"]
-latitude=response.json()["iss_position"]["latitude"]
+print(sunrise)
+print(sunset)
 
-iss_position=(longitude,latitude)
-
-print(iss_position)
+time_now=datetime.now()
+print(time_now.hour)
